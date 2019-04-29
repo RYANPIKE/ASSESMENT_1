c03684b3fa0ff8d6e7db50214c23bd37594c487f
Ryan Pike <ryan.pikey99@gmail.com>
2019-04-29 06:26:36 +0000

    Final Commit

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
 int main()
     {
       int choice; 
       
       Do 
         {
-        printf("List of st of options:\n");                           // 
+        printf("List of st of options:\n");                           // All menu options listed
         printf("--------------------\n");
         printf("1. Encryption with Cesar 1 \n");
         printf("2. Decryption with Cesar 2 \n");
@@ -15,7 +14,7 @@ int main()
         printf("3. Encryption with Substitution (attempt) 3\n");
         printf("--------------------\n");
         
-    printf("Enter number:  \n");
+    printf("Enter number:  \n");                                    // Requests what option is to be chosen 
     scanf("%d", & choice);
    
   
@@ -26,20 +25,18 @@ int main()
             
     case '1':
     
-    char str[]="RYANPIKE"; // Hard coded message to be encrypted
-    int k = 3;
-    //int *temp;
-    //temp  = str[0];
-    int temp;
+        char str[]="RYANPIKE";          // Hard coded message to be encrypted
+        int k = 3;                      // key value stated as 3
+        int temp;
     
-    for (int i = 0; i < 8; i++) {// i incremently gets larger by 1 until it = 8 then breaks
-        temp = str[i]-65;  // string values 0,1,2,3,4,5,6,7 are found by - 65 off ascii values of characters
+        for (int i = 0; i < 8; i++) {       // i incremently gets larger by 1 until = 8 then breaks
+        temp =  str[i]-65;              // string values 0,1,2,3,4,5,6,7 are found by - 65 off ascii values of characters
         temp = (temp + k)%26;
-        temp = temp + 65;
+        temp = temp + 65;               // key value is added which changes ASCII value
         str[i] = temp; 
        
        
-       printf("%d, %c\n",i, str[i]); //the key for encryption is 3 that is added to ASCII value
+       printf("%d, %c\n",i, str[i]);        //the new string is then printed with the new ascii values completeing the encryption
         
     break;
     
@@ -49,63 +46,36 @@ int main()
     
     case '2':
     
-    char str[]="UBDQSLNH"; // Hard coded message to be encrypted
-    int k = 3;
-    //int *temp;
-    //temp  = str[0];
-    int temp;
+        char str[]="UBDQSLNH"; // Hard coded message to be decrypted
+        int k = 3;
+         int temp;
     
-    for (int i = 0; i < 8; i++) {// i incremently gets larger by 1 until it = 8 then breaks
-        temp = str[i]-65;  // string values 0,1,2,3,4,5,6,7 are found by - 65 off ascii values of characters
-        temp = (temp - k +26) %26;
+        for (int i = 0; i < 8; i++) {               // i incremently gets larger by 1 until it = 8 then breaks
+        temp = str[i]-65;                       // string values 0,1,2,3,4,5,6,7 are found by - 65 off ascii values of characters
+        temp = (temp - k +26) %26;              // Key is minused off temporary value 
         temp = temp + 65;
-        str[i] = temp; 
+        str[i] = temp;                          //subtraction of Key sets numbers back to what the original message was
        
        
-       printf("%d, %c\n",i, str[i]); //the key for encryption is 3 that is added to ASCII value
+        printf("%d, %c\n",i, str[i]);             //Decrypted message is printed as original message
    
     break;
  
-  }
-
-    
-
-
-
-int main()
-    {
-        
-        
-    char str[]="UBDQSLNH"; // Hard coded message to be encrypted
-    int k = 3;
-    //int *temp;
-    //temp  = str[0];
-    int temp;
-    
-    for (int i = 0; i < 8; i++) {// i incremently gets larger by 1 until it = 8 then breaks
-        temp = str[i]-65;  // string values 0,1,2,3,4,5,6,7 are found by - 65 off ascii values of characters
-        temp = (temp - k +26) %26;
-        temp = temp + 65;
-        str[i] = temp; 
-       
-       
-       printf("%d, %c\n",i, str[i]); //the key for encryption is 3 that is added to ASCII value
-
+     
    
    case '3':
    
 
-    char Abet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char key[] =  "QWERTYUIOPASDFGHJKLZXCVBNM";
-    char message[] = "TEST";
-    char output[200];
-    int i = 0;
-    int j = 0;
-    
-
-     
-    
-    return 0;
-}  
-    return 0;
+        char Abet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";             //String of numbers is stated as the value boundaries
+        char key[] =  "QWERTYUIOPASDFGHJKLZXCVBNM";             //Key is stated 
+        char message[] = "TEST";                                //Word being encryoted is stated as a char before being run through the key
+        char output[200];
+        int i = 0;
+        int j = 0;
+        .
+        .
+        .
+        . Not finished
+    break;
+}
 }
